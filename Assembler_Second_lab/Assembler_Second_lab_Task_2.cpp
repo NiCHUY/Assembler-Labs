9b02283338b8278abde3e56ae89c6e792a936b5a
NiCHUY <89972933+NiCHUY@users.noreply.github.com>
2022-03-02 21:44:24 +0300

    Assembler Second Lab

---
+++ Assembler_Second_lab/Assembler_Second_lab_Task_2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+int main() {
+
+__int32 number(3), number_in_power_of_2(0), number_of_decades(0), ten_in_power_of_number_of_decades(0);
+__int32 ten(10), result(0);
+
+   __asm{
+
+       mov EAX, number
+       mul number
+       mov number_in_power_of_2, EAX
+
+       mov EAX, number
+       mov EBX, 0
+
+       Start_of_first_cicle:
+            CMP EAX, 0
+            jz Out_of_first_cicle
+            Div ten
+            add EBX, 1
+       Out_of_first_cicle:
+
+       mov number_of_decades, EBX
+       mov EBX, number_of_decades
+       mov EAX, 1
+
+       Start_of_second_cicle:
+            CMP EBX, 0
+            jz Out_of_second_cicle
+            sub EBX, 1
+            mul ten
+       Out_of_second_cicle:
+
+       mov ten_in_power_of_number_of_decades, EAX
+       mov EBX, ten_in_power_of_number_of_decades
+       mov EAX, number_in_power_of_2
+
+       div EBX
+       mov EBX, number
+
+       Start_of_third_cicle:
+            CMP EDX, EBX
+            jz Automorphic
+            jmp Not_Automorphic
+       Automorphic:
+            mov EAX, 1
+            jmp End_of_program
+       Not_Automorphic:
+            mov EAX, 0
+            jmp End_of_program
+       End_of_program:
+            mov result, EAX
+   }
+   if(result == 1){
+       std::cout << "Number is Automorphic" << std::endl;
+   }
+   else{
+       std::cout << "Number is not Automorphic" << std::endl;
+   }
+    return 0;
+}
